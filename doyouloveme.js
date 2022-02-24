2f75cbed4a674e1641cf2c538736c918b3ebb6d0
dunghojava <dungho21397@gmail.com>
2022-02-24 23:29:43 +0700

    do you love me

--- doyouloveme.js
+++ doyouloveme.js
@@ -7,28 +7,16 @@ r = document.getElementById("runno");
 
 function init() {
     r = document.getElementById("runno");
-    r.style.position = 'relative';
+    r.style.position = 'Relative';
     r.style.left = '0px';
     r.style.top = '0px';
-    r.style.right = '0px';
-    r.style.bottom = '0px';
 }
-var width = window.innerWidth
-    || document.documentElement.clientWidth
-    || document.body.clientWidth;
-var height = window.innerHeight
-    || document.documentElement.clientHeight
-    || document.body.clientHeight;
-function no() {
 
-    r.style.left = parseInt(r.style.left) + Math.random() * 200 + 'px';
-    r.style.top = parseInt(r.style.top) + Math.random() * 200 + 'px';
-    r.style.bottom = parseInt(r.style.bottom) + Math.random() * 200 + 'px';
-    r.style.right = parseInt(r.style.right) + Math.random() * 200 + 'px';
-    // if (r.style.left < width && r.style.top < height) {
-    //     r.style.left = parseInt(r.style.left) + Math.random() * 200 + 'px';
-    //     r.style.top = parseInt(r.style.top) + Math.random() * 200 + 'px';
-    // }
+function no() {
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    r.style.left = Math.random() * width + 'px';
+    r.style.top = Math.random() * height + 'px';
 }
 
 window.onload = init;
